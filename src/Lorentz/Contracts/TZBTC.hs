5b2d9915d61c96af4835e736d94c425504418d57
rvem <roman.melnikov@serokell.io>
2019-08-26 12:12:19 +0300

    [TBTC-3] Update CLI
    Problem: There are some other functionality that we want to have
    in our CLI:
    1) Print initial contract storage
    2) Parse raw Michelson contract parameter into humanreadable
    representation
    
    Solution: Add new commands to our CLI for this functionality.
    Print initial contract storage by the given address. And also
    parse given value and print it in humanreadable way.

--- src/Lorentz/Contracts/TZBTC.hs
+++ src/Lorentz/Contracts/TZBTC.hs
@@ -12,6 +12,8 @@ module Lorentz.Contracts.TZBTC
   , tzbtcContract
   ) where
 
+import Fmt (Buildable(..), (+|), (|+))
+
 import Lorentz
 
 import Lorentz.Contracts.TZBTC.Impl
@@ -45,6 +47,48 @@ data Parameter
   | Migrate             !MigrateParams
   deriving stock Generic
   deriving anyclass IsoValue
+
+
+instance Buildable Parameter where
+  build = \case
+    Transfer (arg #from -> from, arg #to -> to, arg #value -> value) ->
+      "Transfer from " +| from |+ " to " +| to |+ ", value = " +| value |+ ""
+    Approve (arg #spender -> spender, arg #value -> value) ->
+      "Approve for " +| spender |+ ", value = " +| value |+ ""
+    GetAllowance (View (arg #owner -> owner, arg #spender -> spender) _) ->
+      "Get allowance for " +| owner |+ " from " +| spender |+ ""
+    GetBalance (View addr _) ->
+      "Get balance for " +| addr |+ ""
+    GetTotalSupply _ ->
+      "Get total supply"
+    SetPause b ->
+      "Set pause to " +| b |+ ""
+    SetAdministrator addr ->
+      "Set administrator to " +| addr |+ ""
+    GetAdministrator _ ->
+      "Get administrator"
+    Mint (arg #to -> to, arg #value -> value) ->
+      "Mint to " +| to |+ ", value = " +| value |+ ""
+    Burn (arg #from -> from, arg #value -> value) ->
+      "Burn from " +| from |+ ", value = " +| value |+ ""
+    AddOperator (arg #operator -> operator) ->
+      "Add operator " +| operator |+ ""
+    RemoveOperator (arg #operator -> operator) ->
+      "Remove operator " +| operator |+ ""
+    SetRedeemAddress (arg #redeem -> redeem) ->
+      "Set redeem address to " +| redeem |+ ""
+    Pause b ->
+      "Pause " +| b |+ ""
+    TransferOwnership (arg #newowner -> newOwner) ->
+      "Transfer ownership to " +| newOwner |+ ""
+    AcceptOwnership _ ->
+      "Accept ownership"
+    StartMigrateTo (arg #migrateto -> migrateTo) ->
+      "Start migrate to " +| migrateTo |+ ""
+    StartMigrateFrom (arg #migratefrom -> migrateFrom) ->
+      "Start migrate from " +| migrateFrom |+ ""
+    Migrate _ ->
+      "Migrate"
 
 ----------------------------------------------------------------------------
 -- Implementation
