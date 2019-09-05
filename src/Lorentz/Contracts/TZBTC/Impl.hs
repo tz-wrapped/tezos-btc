0b802155408382135ffbf2db09a082bba93bd3df
Sandeep.C.R <sandeep.chandrika@serokell.io>
2019-09-05 21:02:05 +0530

    [TBTC-6] Tests for proxy

--- src/Lorentz/Contracts/TZBTC/Impl.hs
+++ src/Lorentz/Contracts/TZBTC/Impl.hs
@@ -394,8 +394,7 @@ setProxy = do
   right @Address
   dip $ getField #fields
   setField #proxy
-  setField #fields
-  nil; pair
+  setFields
 
 -- | Check that the sender is admin
 authorizeAdmin
--- test/Test/Proxy.hs
+++ test/Test/Proxy.hs
@@ -6,34 +6,25 @@ module Test.Proxy
   ( test_proxy
   ) where
 
-import Fmt (pretty)
 import Test.Tasty (TestTree, testGroup)
-import Test.Tasty.HUnit (Assertion, assertEqual, assertBool, assertFailure, testCase)
+import Test.Tasty.HUnit (testCase)
 import qualified Data.Map as Map
 import Data.Set
-import Data.Singletons (SingI(..))
-import qualified Data.Set as Set
-import Named (arg)
 
 import Lorentz
 import Lorentz.Contracts.Consumer
 import Lorentz.Contracts.TZBTC
-import qualified Lorentz.Contracts.TZBTC.Agent as Agent
-import Lorentz.Contracts.TZBTC.Types
 import Lorentz.Test.Integrational
-import Michelson.Interpret (ContractEnv(..), MichelsonFailed(..))
-import Michelson.Test (ContractPropValidator, contractProp, dummyContractEnv, IntegrationalScenarioM)
-import Michelson.Text (mt)
-import Michelson.Typed (Instr, InstrWrapC, AppendCtorField, GetCtorField, ToTs, Value, Value'(..))
-import Michelson.Typed.Scope (checkOpPresence, OpPresence(..))
+import Michelson.Test (IntegrationalScenarioM)
 import Util.Named
 
 import qualified Lorentz.Contracts.TZBTC.Proxy as Proxy
+import Lorentz.Contracts.TZBTC.Proxy (fromSaneParameter, SaneParameter(..))
 import qualified Lorentz.Contracts.TZBTC as TZBTC
 
 originateContract :: IntegrationalScenarioM (ContractAddr Parameter)
 originateContract =
-  lOriginate tzbtcContract "UserUpgradeable V1" storage (toMutez 1000)
+  lOriginate tzbtcContract "TZBTC Contract" storage (toMutez 1000)
 
 adminAddress :: Address
 adminAddress = genesisAddress3
@@ -44,23 +35,99 @@ redeemAddress_ = adminAddress
 initialSupply :: Natural
 initialSupply = 500
 
+alice :: Address
+alice = genesisAddress6
+
+bob :: Address
+bob = genesisAddress5
+
 storage :: Storage
 storage =
   mkStorage adminAddress redeemAddress_
     (Map.fromList [(redeemAddress_, initialSupply)]) mempty
 
-originateProxy :: ContractAddr TZBTC.Parameter -> IntegrationalScenarioM (ContractAddr Proxy.Parameter)
+originateProxy
+  :: ContractAddr TZBTC.Parameter
+  -> IntegrationalScenarioM (ContractAddr Proxy.Parameter)
 originateProxy targetAddress =
   lOriginate Proxy.tzbtcProxyContract "TZBTC Proxy" targetAddress (toMutez 1000)
+
+originateAndSetProxy
+  :: IntegrationalScenarioM (ContractAddr Parameter, ContractAddr Proxy.Parameter)
+originateAndSetProxy = do
+  c <- originateContract
+  p <- originateProxy c
+  withSender adminAddress $ lCall c (SetProxy $ unContractAddress p)
+  pure (c, p)
 
 test_proxy :: TestTree
 test_proxy = testGroup "TZBTC contract proxy origination test"
   [ testCase
-      "Proxy saves the address of target contract" $
+      "Proxy forwards calls to transfer entry points properly" $
         integrationalTestExpectation $ do
-          c <- originateContract
-          proxy <- originateProxy c
+          (c, proxy) <- originateAndSetProxy
+          -- Use proxy to transfer 100 tokens from redeemAddress to
+          -- alice
+          withSender redeemAddress_ $ lCall proxy
+            (fromSaneParameter $
+              STransfer (#from .! redeemAddress_, #to .! alice, #value 100))
+          consumer <- lOriginateEmpty contractConsumer "consumer"
+          -- Use proxy to get balance in redeemAddress_ and alice
+          -- accounts.
+          lCall proxy
+            (fromSaneParameter $
+              SGetBalance (View redeemAddress_ consumer))
+          lCall proxy
+            (fromSaneParameter $
+              SGetBalance (View alice consumer))
+          -- Use proxy to get total supply
+          -- accounts.
+          lCall proxy
+            (fromSaneParameter $
+              SGetTotalSupply (View () consumer))
           validate . Right $
-            lExpectStorageConst proxy (unContractAddress c)
+            lExpectViewConsumerStorage consumer [400, 100, 500]
+  , testCase
+      "Proxy forwards calls to getAllowance entry points properly" $
+        integrationalTestExpectation $ do
+          (c, proxy) <- originateAndSetProxy
+          -- Use proxy to transfer of 100 tokens from redeemAddress to
+          -- alice
+          withSender redeemAddress_ $ lCall proxy
+            (fromSaneParameter $
+              STransfer (#from .! redeemAddress_, #to .! alice, #value 100))
+          -- Use proxy to approve transfer of 20 tokens from alice to
+          -- bob
+          withSender alice $ lCall proxy
+            (fromSaneParameter $
+              SApprove (#spender .! bob, #value 20))
+
+          consumer <- lOriginateEmpty contractConsumer "consumer"
+          -- Use proxy to get allowance for alice to bob transfer
+          withSender alice $ lCall proxy
+            (fromSaneParameter $
+              SGetAllowance (View (#owner .! alice, #spender .! bob) consumer))
+
+          -- Use proxy to transfer 10 tokens from alice to
+          -- bob, as bob.
+          withSender bob $ lCall proxy
+            (fromSaneParameter $
+              STransfer (#from .! alice, #to .! bob, #value 15))
+
+          -- Use proxy to get balance in alice and bob
+          -- accounts.
+          lCall proxy
+            (fromSaneParameter $
+              SGetBalance (View alice consumer))
+          lCall proxy
+            (fromSaneParameter $
+              SGetBalance (View bob consumer))
+
+          -- Again, use proxy to get allowance for alice to bob transfer
+          lCall proxy
+            (fromSaneParameter $
+              SGetAllowance (View (#owner .! alice, #spender .! bob) consumer))
+          validate . Right $
+            lExpectViewConsumerStorage consumer [20, 85, 15, 5]
   ]
 
