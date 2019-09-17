52e24827b994341b31e82da6bfa57f1229eee2a6
rvem <roman.melnikov@serokell.io>
2019-09-17 15:19:02 +0300

    fixup! mda

--- kek.py
+++ kek.py
@@ -3,11 +3,9 @@
 # SPDX-License-Identifier: LicenseRef-Proprietary
 #
 
-license = """
-- SPDX-FileCopyrightText: 2019 Example
--
-- SPDX-License-Identifier: LicenseRef-Proprietary
-"""
+license =
+"- SPDX-FileCopyrightText: 2019 Example\n" +
+"- SPDX-License-Identifier: LicenseRef-Proprietary"
 
 fout = open('out.txt', 'w')
 fout.write(license)
