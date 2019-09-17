c2a27c11d2697e830c9237ab6e7f48620e66a07b
rvem <roman.melnikov@serokell.io>
2019-09-17 15:06:14 +0300

    mda

---
+++ kek.py
@@ -0,0 +1,14 @@
+# SPDX-FileCopyrightText: 2019 Example
+#
+# SPDX-License-Identifier: LicenseRef-Proprietary
+#
+
+license = """
+- SPDX-FileCopyrightText: 2019 Example
+-
+- SPDX-License-Identifier: LicenseRef-Proprietary
+"""
+
+fout = open('out.txt', 'w')
+fout.write(license)
+fout.close()
