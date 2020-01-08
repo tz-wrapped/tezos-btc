1f2fe71b96cae74ff1e00001210747a666be1574
rvem <roman.melnikov@serokell.io>
2020-01-08 17:47:27 +0300

    [TBTC-77] Produce patched tezos-client in CI
    Problem: In order to sign multisig packages using ledger device will
    have to used patched `tezos-client` executable due to the fact that
    existing unpatched `tezos-client` is incapable in signing serialized
    Michelson values (bytesequences which have 0x05 prefix).
    We don't really want patched binary to be visible in
    tezos-packaging repo, also, it is convenient to provide these patched
    binaries as a release artifacts.
    
    Solution: Add `tezos-client-{mainnet, babylonnet}` targets to
    `release.nix` and produce patched binaries in CI.

--- release.nix
+++ release.nix
@@ -2,8 +2,7 @@
 #
 # SPDX-License-Identifier: LicenseRef-Proprietary
 let sources = import ./nix/sources.nix;
-in
-{ pkgs ? import sources.nixpkgs { } }:
+in { pkgs ? import sources.nixpkgs { } }:
 with pkgs;
 
 let
@@ -32,7 +31,27 @@ let
   ubuntuImage = makeImageFromDebDist debDistros.ubuntu1804x86_64;
   fedoraImage = makeImageFromRPMDist rpmDistros.fedora27x86_64;
 
+  mainnetInfo = {
+    url = "https://gitlab.com/serokell/morley/tezos.git";
+    rev = "e0ab0c74"; # rvem/mainnet-client-patched
+    sha256 = "0b5mp7gms31qnqybph1r82v60sx6scigrv0w7y6r9ssb0v23vr4y";
+  };
+
+  babylonnetInfo = {
+    url = "https://gitlab.com/serokell/morley/tezos.git";
+    rev = "dd8a9c6b"; # rvem/babylonnet-client-patched
+    sha256 = "1bpck6q1cgzkhbmgx16m118qff3rs2ndvrzq9bh2hzd6iqww75mj";
+  };
+
+  tezosPackaging = import (fetchFromGitHub {
+    owner = "serokell";
+    repo = "tezos-packaging";
+    rev = "126f6dff28d255058c05bfd917bd6fb5988dd185";
+    sha256 = "1pk5q5q51lgzic6pvvvnb0vskwy7vf2h90461fg3rmvxpzaz2c57";
+  }) { inherit mainnetInfo babylonnetInfo; };
+
 in rec {
+  inherit (tezosPackaging) tezos-client-mainnet tezos-client-babylonnet;
   static = tzbtc-static;
   rpm = vmTools.runInLinuxImage
     (buildRpm.packageRpm // { diskImage = fedoraImage; });
