b00f195a5ec9d54356bdf0f7d48550c12c9adcb0
rvem <roman.melnikov@serokell.io>
2020-02-17 16:00:50 +0300

    [TBTC-90] Bump haskell.nix
    Problem: `default.nix` duplicates `stack.yaml`, it is easy to forget to
    update the former, when updating the latter. `cache` became redundant in
    fresh revision of haskell.nix.
    
    Solution: Bump haskell.nix revision, remove cache.

--- default.nix
+++ default.nix
@@ -27,12 +27,6 @@ let
       inherit (p) stack-nix;
       shells.ghc = p.hsPkgs.shellFor { };
     };
-  morley-repo = {
-    name = "morley";
-    url = "https://gitlab.com/morley-framework/morley.git";
-    rev = "960b8c79e4d2e241d4600f8d2c6e82ff2cb9c8fe";
-    sha256 = "0q42bsd8f4chmrsziw037kxsclwwj5cym708jkw2pmy5l98hnphh";
-  };
   gpl = true; # false: not supported yet
   tezos-btc = { buildPkgs, hostPkgs }:
     stackProjectCross {
@@ -57,58 +51,6 @@ let
             "--ghc-option=-optl=-static"
           ] ++ map (drv: "--ghc-option=-optl=-L${drv}/lib") staticLibs;
       }];
-      cache = [
-        morley-repo
-        (morley-repo // rec {
-          name = "morley-prelude";
-          subdir = "prelude";
-        })
-        (morley-repo // rec {
-          name = "morley-ledgers";
-          subdir = name;
-        })
-        (morley-repo // rec {
-          name = "morley-ledgers-test";
-          subdir = name;
-        })
-        (morley-repo // rec {
-          name = "morley-multisig";
-          subdir = name;
-        })
-        (morley-repo // rec {
-          name = "morley-upgradeable";
-          subdir = name;
-        })
-        (morley-repo // rec {
-          name = "morley-nettest";
-          subdir = name;
-        })
-        (morley-repo // rec {
-          name = "indigo";
-          subdir = name;
-        })
-        (morley-repo // rec {
-          name = "tasty-hunit-compat";
-          subdir = name;
-        })
-
-        rec {
-          name = "tezos-bake-monitor-lib";
-          url =
-            "https://gitlab.com/obsidian.systems/tezos-bake-monitor-lib.git";
-          rev = "19a9ce57a0510bc3ad8a3f639d0a968a65024b86";
-          sha256 = "0ypf7z2c9w59rz7hymzdyx87783qdfp3kygs9jl8qnmbfslmi8jr";
-          subdir = name;
-        }
-        # morley-nettest depends on caps
-        rec {
-            name = "caps";
-            url =
-              "https://github.com/int-index/caps.git";
-            rev = "ab4345eabd58fc6f05d3b46bea2c5acdba3ec6f8";
-            sha256 = "0r1zqa559gaxavsd8zynlpa2c5hi2qc20n8s4bhcdaw36hasg3kr";
-        }
-      ];
       crossPkgs = buildPkgs;
       pkgs = hostPkgs;
     };
