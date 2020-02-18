999e1b958de2710c86a145ace387b6297cf8e865
Alexander Bantyev <balsoft@balsoft.ru>
2020-02-18 17:31:19 +0300

    [TBTC-90] Refactor repository & fix haddock issues

--- default.nix
+++ default.nix
@@ -1,61 +1,31 @@
 # SPDX-FileCopyrightText: 2019 Bitcoin Suisse
 #
 # SPDX-License-Identifier: LicenseRef-Proprietary
+{ static ? false }:
 let
   sources = import ./nix/sources.nix;
   haskell-nix = import sources."haskell.nix";
-  pkgs-native = import sources.nixpkgs haskell-nix;
-  inherit (pkgs-native) lib;
-  pkgs-musl = import sources.nixpkgs
-    (haskell-nix // { crossSystem = lib.systems.examples.musl64; });
-  stackProjectCross = { crossPkgs, pkgs, ... }@args:
-    with crossPkgs.haskell-nix;
-    let
-      stack = importAndFilterProject (pkgs.haskell-nix.callStackToNix args);
-      pkg-set = mkStackPkgSet {
-        stack-pkgs = stack.pkgs;
-        pkg-def-extras = (args.pkg-def-extras or [ ]);
-        modules = (args.modules or [ ])
-          ++ lib.optional (args ? ghc) { ghc.package = args.ghc; }
-          ++ lib.optional (args ? cache) (mkCacheModule args.cache);
-      };
-      p = {
-        inherit (pkg-set.config) hsPkgs;
-        stack-nix = stack.nix;
-      };
-    in p.hsPkgs // {
-      inherit (p) stack-nix;
-      shells.ghc = p.hsPkgs.shellFor { };
-    };
-  gpl = true; # false: not supported yet
-  tezos-btc = { buildPkgs, hostPkgs }:
-    stackProjectCross {
-      src = hostPkgs.haskell-nix.haskellLib.cleanGit { src = ./.; };
-      modules = let
-        staticLibs = with buildPkgs;
-          [
-            zlib.static
-            (libffi.overrideAttrs (oldAttrs: {
-              dontDisableStatic = true;
-              configureFlags = (oldAttrs.configureFlags or [ ])
-                ++ [ "--enable-static" "--disable-shared" ];
-            }))
-          ] ++ lib.optional gpl (gmp6.override { withStatic = true; });
-      in [{
-        packages.tzbtc.components.exes.tzbtc-client.configureFlags =
-          with buildPkgs;
-          lib.optionals stdenv.hostPlatform.isMusl [
-            "--disable-executable-dynamic"
-            "--disable-shared"
-            "--ghc-option=-optl=-pthread"
-            "--ghc-option=-optl=-static"
-          ] ++ map (drv: "--ghc-option=-optl=-L${drv}/lib") staticLibs;
-      }];
-      crossPkgs = buildPkgs;
-      pkgs = hostPkgs;
-    };
-in { static ? false }:
-(tezos-btc {
-  hostPkgs = pkgs-native;
-  buildPkgs = if static then pkgs-musl else pkgs-native;
-}).tzbtc.components.exes.tzbtc-client
+  nixpkgs = import sources.nixpkgs haskell-nix;
+  hn = if static then
+    nixpkgs.pkgsCross.musl64.haskell-nix
+  else
+    nixpkgs.haskell-nix;
+  inherit (nixpkgs) lib;
+  project = hn.stackProject {
+    src = hn.haskellLib.cleanGit { src = ./.; };
+    modules = let
+      staticLibs = with nixpkgs.pkgsStatic; [
+        zlib
+        gmp6
+        libffi
+      ];
+    in [{
+      packages.tzbtc.configureFlags = lib.optionals static [
+        "--disable-executable-dynamic"
+        "--disable-shared"
+        "--ghc-option=-optl=-pthread"
+        "--ghc-option=-optl=-static"
+      ] ++ map (drv: "--ghc-option=-optl=-L${drv}/lib") staticLibs;
+    }];
+  };
+in project.tzbtc.components.exes.tzbtc-client
